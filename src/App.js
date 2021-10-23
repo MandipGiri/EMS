bea85006d9fa0f6aebe2da6b105e255b33793030
Mandip Giri <mandipgiri@Mandips-MacBook-Pro.local>
2021-10-24 00:36:45 +0545

    frontend setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { STORAGE_CONSTANTS } from "./utilities/StorageConstants";
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem(STORAGE_CONSTANTS.ACCESS_TOKEN) ?? "";
+    if (!!token) setisLoggedIn(true);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {isLoggedIn ? "HELLO WORLD Logged IN" : "HELLO WORLD"}
     </div>
   );
 }
