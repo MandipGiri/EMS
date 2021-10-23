d4fddb693a73bef0e9580b067e7ddc824c76ff5e
Mandip Giri <mandipgiri@Mandips-MacBook-Pro.local>
2021-10-23 10:15:48 +0545

    :sparkle: :art: node_modules for backend  addition to git ignore

--- ems-backend/routes/users.js
+++ ems-backend/routes/users.js
@@ -7,6 +7,7 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+/* add new user */
 router.route("/add").post((req, res) => {
   const fullName = req.body.userName;
   const dateOfBirth = req.body.dateOfBirth;
